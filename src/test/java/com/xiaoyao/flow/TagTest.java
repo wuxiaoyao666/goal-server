2b25b84f425e1f22b5b42486851828b8a498492d
wuyusen <1214166598@qq.com>
2025-03-23 17:28:10 +0800

    测试取消

--- src/test/java/com/xiaoyao/flow/TagTest.java
+++ src/test/java/com/xiaoyao/flow/TagTest.java
@@ -1,8 +1,6 @@
 package com.xiaoyao.flow;
 
-import com.xiaoyao.flow.service.ITagService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -11,11 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TagTest {
 
-    @Autowired
-    ITagService tagService;
-
     @Test
     void loadTag(){
-        tagService.list().forEach(System.out::println);
+
     }
 }
