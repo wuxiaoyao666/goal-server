9f1c7d78b871fc8def1a1acf1b645b5258c0a929
wuyusen <1214166598@qq.com>
2025-03-29 23:34:20 +0800

    feat:针对用户进行操作

--- src/main/java/com/xiaoyao/flow/service/impl/UserServiceImpl.java
+++ src/main/java/com/xiaoyao/flow/service/impl/UserServiceImpl.java
@@ -1,15 +1,11 @@
 package com.xiaoyao.flow.service.impl;
 
-import cn.hutool.core.io.FileUtil;
-import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.xiaoyao.flow.entity.Tag;
 import com.xiaoyao.flow.entity.User;
 import com.xiaoyao.flow.entity.dto.RegisterDTO;
 import com.xiaoyao.flow.exception.BusinessException;
 import com.xiaoyao.flow.mapper.UserMapper;
-import com.xiaoyao.flow.service.ITagService;
 import com.xiaoyao.flow.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xiaoyao.flow.utils.BcryptUtils;
@@ -17,11 +13,7 @@ import com.xiaoyao.flow.utils.ResultCode;
 import com.xiaoyao.flow.entity.vo.UserVo;
 import com.xiaoyao.flow.entity.dto.LoginDTO;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 /**
  * <p>
@@ -33,9 +25,6 @@ import java.util.List;
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
-
-    @Autowired
-    private ITagService tagService;
 
     @Override
     public UserVo login(LoginDTO param) {
@@ -54,18 +43,10 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     }
 
     @Override
-    @Transactional
     public void register(RegisterDTO body) {
         User user = new User();
         BeanUtils.copyProperties(body, user);
         user.setPassword(BcryptUtils.hash(user.getPassword()));
         save(user);
-        // 初始化 Tag
-        String initializeTags = FileUtil.readUtf8String("initialize/initialize_tag.json");
-        List<Tag> tags = JSON.parseArray(initializeTags, Tag.class);
-        tags.forEach(tag -> {
-            tag.setUserId(user.getId());
-        });
-        tagService.saveBatch(tags);
     }
 }
