f891ed32b29f1a8d629e254141f07b886c3b0a55
wuyusen <1214166598@qq.com>
2025-10-07 22:30:16 +0800

    优化脚本

--- src/main/java/com/xiaoyao/goal/Application.java
+++ src/main/java/com/xiaoyao/goal/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class Application {
-    void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
